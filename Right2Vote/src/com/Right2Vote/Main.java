611acd656889952fd7341d2cfbe41321c9729f53
abhishekyadav0888 <shraddhayadav0888.com>
2023-10-16 12:21:27 +0530

    Done

---
+++ Right2Vote/src/com/Right2Vote/Main.java
@@ -0,0 +1,30 @@
+package com.Right2Vote;
+
+public class Main {
+	
+	public static void main (String[] args) {
+		int budget = 600;
+
+        double Tshirt = 50.00;
+        double Jeans = 80.00;
+        double Jacket = 100.00;
+        
+        findCombination(budget, Tshirt, Jeans, Jacket);
+	}
+	
+	public static void findCombination(int budget, double Tshirt, double Jeans, double Jacket) {
+        int maxTshirt = (int) (budget / Tshirt);
+        int maxJeans = (int) (budget / Jeans);
+        int maxJacket = (int) (budget / Jacket);
+
+        for (int i = 1; i <= maxTshirt; i++) {
+            for (int j = 1; j <= maxJeans; j++) {
+                int k = (int) Math.min(maxJacket, (budget - (i * Tshirt + j * Jeans)) / Jacket);
+                double totalcost = i*Tshirt + j*Jeans + k*Jacket;
+                if(i>=1 && j>=1 && k>=1 && totalcost<=budget && totalcost>=(budget- Math.min(Tshirt, (Math.min(Jeans, Jacket))))) {
+                    System.out.println("Tshirt = " + i + " Jeans = " + j + " Jacket = " + k);
+                }
+            }
+        }
+    }
+}
